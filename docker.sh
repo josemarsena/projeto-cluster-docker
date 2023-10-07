32103b76cfd2c333f353f24bf9e19a348959db8b
josemarsena <josemarsena@users.noreply.github.com>
2023-10-07 20:18:27 -0300

    Update docker.sh
    Atualizado o Script para Instalar o Docker Compose versão 2.3.3 segundo Digital Ocean

--- docker.sh
+++ docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com | sudo bash
-sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Alterar a Instalação do Docker Compose
+# sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo mkdir -p ~/.docker/cli-plugins/
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker vagrant
